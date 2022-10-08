435df5eef3350891928265d7a86ece93b9607eee
Archie Sage <archie.sage@kcl.ac.uk>
2022-10-08 09:51:59 +0100

    Fix validators syntax for quantity model field

--- things/models.py
+++ things/models.py
@@ -4,4 +4,7 @@ from django.core.validators import RegexValidator, MaxValueValidator, MinValueVa
 class Thing(models.Model):
     name = models.CharField(unique=True, blank=False, max_length=30)
     description = models.CharField(blank=True, max_length=120)
-    quantity = models.IntegerField(MaxValueValidator(100), MinValueValidator(0))
+    quantity = models.IntegerField(unique=False, validators = [
+        MinValueValidator(0),
+        MaxValueValidator(101)
+    ])
